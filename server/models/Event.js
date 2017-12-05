301aa444445effaa2ff39360cb08ddcd3c07256a
acampos91 <acampos91@hotmail.com>
2017-12-05 16:44:59 +0100

    models completed

---
+++ server/models/Event.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+const TYPES = require('./Trip-types');
+
+const eventSchema = new Schema({
+  name: String,
+  creator: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+  participants: [],
+  description: String,
+  tripId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Trip'
+    },
+  startDate: Date,
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+
+});
+const Event = mongoose.model("Event", eventSchema);
+module.exports = Event;
---
+++ server/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: String,
+  lastname: String,
+  username: String,
+  password: String,
+  typeOfVan: String,
+  photo: String,
+  location: {
+      latitude: Number,
+      longitude: Number,
+      description: String
+    },
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+
+});
+const User = mongoose.model("User", userSchema);
+module.exports = User;
